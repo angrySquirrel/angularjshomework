16daea08a37a396d3e7766b85a24409be47071e0
angrySquirrel <1043030172@qq.com>
2016-10-04 13:36:55 +0800

    new code

--- module3-solution/js/app.js
+++ module3-solution/js/app.js
@@ -1,63 +1,38 @@
-
 (function(){
 'use strict';
 
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
-.directive('foundItems', FoundItemsDirective)
+.directive('foundItems', FoundItems)
 .constant('baseURL', 'https://davids-restaurant.herokuapp.com/');
-
-function FoundItemsDirective() {
-    var ddo = {
-        templateUrl: './templates/found-items.html',
-        scope: {
-            items: '<',
-            onRemove: '&',
-        },
-        controller: FoundItemsController,
-        controllerAs: 'ctrler',
-        bindToController: true,
-    };
-
-    return ddo;
-}
-
-function FoundItemsController() {
-    var ctrler = this;
-}
 
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService) {
-    var nidCtrler = this;//save context
-    nidCtrler.queryString = '';
-    nidCtrler.message = '';
-    nidCtrler.foundItems = [];
-
-    nidCtrler.narrowItDown = narrowItDown;
-    nidCtrler.removeItem = removeItem;
-
-    function narrowItDown() {
-        var str = nidCtrler.queryString.trim();
+    var myCtrl = this;
+    myCtrl.foundResults = [];
+    myCtrl.queryString = '';
+    myCtrl.message = '';
+    myCtrl.getFilteredResult = function(){
+        var str = myCtrl.queryString.trim();
         if (str) {
             var promise = MenuSearchService.getMatchedMenuItems(str);
 
             promise.then(function (result) {
-                nidCtrler.foundItems = result;
-                nidCtrler.message = (nidCtrler.foundItems.length) ?
-                    '' : 'Sorry, no matches found';
+                myCtrl.foundResults = result;
+                myCtrl.message = (myCtrl.foundResults.length) ?
+                    '' : 'no match found for your query';
             })
             .catch(function (err) {
-                nidCtrler.message = 'Sorry, failed to retrieve items, please check internet connection';
+                myCtrl.message = 'fail to retrieve the items required';
             });
         } else {
-            nidCtrler.message = 'Empty String, please type in the query first';
+            myCtrl.message = 'empty string, please type in a query first';
         }
     }
-
-    function removeItem(index) {
-        if(nidCtrler.foundItems[index] != undefined){
-            var item = nidCtrler.foundItems.splice(index, 1);
+    myCtrl.removeItem = function(index){
+        if (myCtrl.foundResults[index] != undefined) {
+            var item = myCtrl.foundResults.splice(index, 1);
             return item;
         }
     }
@@ -67,54 +42,50 @@ MenuSearchService.$inject = ['$http', '$q','baseURL'];
 function MenuSearchService($http, $q, baseURL) {
     var service = this;
 
-    service.getMatchedMenuItems = getMatchedMenuItems;
-
-    var menuItems; // menuItems cache
-
-    function getMenuItems() {
-        if (!menuItems) {
-            var response = $http({
-                method: 'GET',
-                url: (baseURL + 'menu_items.json'),
-                params: {},
-            });
-            return response;
-        } else {
-            var deferred = $q.defer();
-            deferred.resolve({
-                data: {
-                    menu_items: menuItems,
-                },
-            });
-            return deferred.promise;
-        }
+    service.getItems = function(){
+        var response = $http({
+            method: 'GET',
+            url: (baseURL + 'menu_items.json'),
+        });
+        return response;
     }
 
-    function checkForMatch(menuItems, str) {
-        var found = []; // array of matches to be returned
-        for (var i = 0; i < menuItems.length; i++) {
-            var item = menuItems[i];
+    service.checkMatch = function(str){ 
+        var filtered = []; 
+        for (var i = 0; i < service.menuItems.length; i++) {
+            var item = service.menuItems[i];
             if (item.description.indexOf(str) != -1) {
-                found.push(item);
+                filtered.push(item);
             }
         }
-        return found;
+        return filtered;
     }
 
-    function getMatchedMenuItems(str) {
+    service.getMatchedMenuItems = function(str){
         var deferred = $q.defer();
-
-        var promise = getMenuItems(); // retrive all menu items
+        var promise = service.getItems();
         promise.then(function(response) {
-            menuItems = response.data.menu_items;
-            var itemsFound = checkForMatch(menuItems, str);
-            deferred.resolve(itemsFound);
+            service.menuItems = response.data.menu_items;
+            var matchedItems = service.checkMatch(str);
+            deferred.resolve(matchedItems);
         })
         .catch(function(err) {
             deferred.reject(err);
         });
-
-        return deferred.promise; //return a promise
-    }
+        return deferred.promise;
+    };
 }
+
+function FoundItems() {
+    var ddo = {
+        restrict: 'E',
+        templateUrl: './foundItems.html',
+        scope: {
+            foundItems: '<',
+            onRemove: '&',
+        },
+    };
+    return ddo;
+}
+
 })();
